bae9bb506c83bb43cf79be5c04796bc5d36cba59
Heretic14 <kursantshk@mail.ru>
2023-11-19 22:40:45 +0700

    Renamed libriary Class1.cs to AreaCalculator.cs Added method Area for calculating circle area
    Added demonstration console app Demonstration.csproj

---
+++ AreaCalculation/AreaCalculation/AreaCalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace AreaCalculation
+{
+    public static class AreaCalculator
+    {
+        /// <summary>
+        /// Calculate circle area
+        /// </summary>
+        /// <param name="r">circle radius</param>
+        /// <returns></returns>
+        public static double Area(int r)
+        {
+            return Math.PI * Math.Pow(r, 2);
+        }
+    }
+}
---
+++ AreaCalculation/Demonstration/Program.cs
@@ -0,0 +1,9 @@
+﻿using AreaCalculation;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(AreaCalculator.Area(4));
+    }
+}
