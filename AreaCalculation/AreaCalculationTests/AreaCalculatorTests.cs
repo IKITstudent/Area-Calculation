08e3ab65a5a6ae7604152445f7cb8f2c1d53b0b4
Heretic14 <kursantshk@mail.ru>
2023-11-20 00:31:40 +0700

    Create Test project
    # Conflicts:
    #	AreaCalculation/Demonstration/Program.cs

---
+++ AreaCalculation/AreaCalculationTests/AreaCalculatorTests.cs
@@ -0,0 +1,12 @@
+namespace AreaCalculation.Tests
+{
+    [TestClass]
+    public class AreaCalculatorTests
+    {
+        [TestMethod]
+        public void Area_ZeroValue_ZeroValue()
+        {
+
+        }
+    }
+}
