36ee7ec5236b467133c6bfaac10725a2b1de5b5b
IKITstudent <KursantSHK@mail.ru>
2023-11-21 20:17:23 +0700

    Added creating tables

--- Task 3 SQL/Task3_SQL-query.sql
+++ Task 3 SQL/Task3_SQL-query.sql
@@ -7,3 +7,27 @@ BEGIN
 	DROP DATABASE Task3DB
 	CREATE DATABASE Task3DB
 END;
+
+GO
+USE Task3DB;
+
+CREATE TABLE Product
+(
+	id INT not null IDENTITY(1,1),
+	product_title VARCHAR(30) not null,
+	PRIMARY KEY (id)
+)
+
+CREATE TABLE Category
+(
+	id INT not null IDENTITY(1,1),
+	category_title VARCHAR(20) not null,
+	PRIMARY KEY (id)
+)
+
+CREATE TABLE Showcase
+(
+	product_ID INT FOREIGN KEY REFERENCES Product(id) not null,
+	category_ID INT FOREIGN key REFERENCES Category(id) not null,
+	PRIMARY KEY (product_ID, category_ID)
+)
